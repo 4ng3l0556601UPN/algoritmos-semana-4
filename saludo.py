358fb1c513cb142c629744c39c5ff228afba34de
4ng3l0556601UPN <n00556601@upn.pe>
2026-06-16 22:06:29 -0500

    Ejercicio #1 Funcion saludar

---
+++ saludo.py
@@ -0,0 +1,8 @@
+### 1. Función void que muestra saludo
+
+def saludar(nombre):
+    print(f"Hola, {nombre}! Bienvenido.")
+
+nombre = input("Ingrese su nombre: ")
+# Llamada a la función para mostrar el saludo
+saludar(nombre)
