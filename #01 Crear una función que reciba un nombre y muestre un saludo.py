753a5115fe864ffaa01f3b73e7405729821ef99a
wandarm <119546115+wandarm@users.noreply.github.com>
2026-06-18 18:25:36 -0500

    Add files via upload

---
+++ #01 Crear una función que reciba un nombre y muestre un saludo.py
@@ -0,0 +1,9 @@
+
+#Crear una función que reciba un nombre y muestre un saludo.
+def saludar(nombre): #defino la función "saludar"  
+    #nombre es un parámetro temporal que recibe el name de la persona
+    print("Hola", nombre) #nombre se considera variable al no tener comillas
+#se usa def para definir una función
+saludar("User") #se ejecuta la función
+
+#Corresponde a L (Local) de LEGB porque el parámetro nombre solo puede utilizarse dentro de la función
